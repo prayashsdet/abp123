02001dd1fab5146818b48feba0155476576b148e
prashant <prashant@192.168.25.175>
2022-01-13 11:49:14 +0530

    LIve tv update

--- src/main/java/com/genericUtility/Swipe.java
+++ src/main/java/com/genericUtility/Swipe.java
@@ -11,66 +11,66 @@ public class Swipe {
 	private static final String UP = null;
 	private static final String LEFT = null;
 
-	public static void swipe(AndroidDriver driver, String direction, long duration) {
-	    Dimension size = driver.manage().window().getSize();
-
-	    int startX = 0;
-	    int endX = 0;
-	    int startY = 0;
-	    int endY = 0;
-
-	    switch (direction) {
-	        case RIGHT:
-	            startY = (int) (size.height / 2);
-	            startX = (int) (size.width * 0.90);
-	            endX = (int) (size.width * 0.05);
-	            new TouchAction(driver)
-	                    .press(startX, startY)
-	                    .waitAction(Duration.ofMillis(duration))
-	                    .moveTo(endX, startY)
-	                    .release()
-	                    .perform();
-	            break;
-
-	        case LEFT:
-	            startY = (int) (size.height / 2);
-	            startX = (int) (size.width * 0.05);
-	            endX = (int) (size.width * 0.90);
-	            new TouchAction(driver)
-	                    .press(startX, startY)
-	                    .waitAction(Duration.ofMillis(duration))
-	                    .moveTo(endX, startY)
-	                    .release()
-	                    .perform();
-
-	            break;
-
-	        case UP:
-	            endY = (int) (size.height * 0.70);
-	            startY = (int) (size.height * 0.30);
-	            startX = (size.width / 2);
-	            new TouchAction(driver)
-	                    .press(startX, startY)
-	                    .waitAction(Duration.ofMillis(duration))
-	                    .moveTo(startX, endY)
-	                    .release()
-	                    .perform();
-	            break;
-
-
-	        case DOWN:
-	            startY = (int) (size.height * 0.70);
-	            endY = (int) (size.height * 0.30);
-	            startX = (size.width / 2);
-	            new TouchAction(driver)
-	                    .press(startX, startY)
-	                    .waitAction(Duration.ofMillis(duration))
-	                    .moveTo(startX, endY)
-	                    .release()
-	                    .perform();
-
-	            break;
-
-	    }
-	}
+//	public static void swipe(AndroidDriver driver, String direction, long duration) {
+//	    Dimension size = driver.manage().window().getSize();
+//
+//	    int startX = 0;
+//	    int endX = 0;
+//	    int startY = 0;
+//	    int endY = 0;
+//
+//	    switch (direction) {
+//	        case RIGHT:
+//	            startY = (int) (size.height / 2);
+//	            startX = (int) (size.width * 0.90);
+//	            endX = (int) (size.width * 0.05);
+//	            new TouchAction(driver)
+//	                    .press(startX, startY)
+//	                    .waitAction(Duration.ofMillis(duration))
+//	                    .moveTo(endX, startY)
+//	                    .release()
+//	                    .perform();
+//	            break;
+//
+//	        case LEFT:
+//	            startY = (int) (size.height / 2);
+//	            startX = (int) (size.width * 0.05);
+//	            endX = (int) (size.width * 0.90);
+//	            new TouchAction(driver)
+//	                    .press(startX, startY)
+//	                    .waitAction(Duration.ofMillis(duration))
+//	                    .moveTo(endX, startY)
+//	                    .release()
+//	                    .perform();
+//
+//	            break;
+//
+//	        case UP:
+//	            endY = (int) (size.height * 0.70);
+//	            startY = (int) (size.height * 0.30);
+//	            startX = (size.width / 2);
+//	            new TouchAction(driver)
+//	                    .press(startX, startY)
+//	                    .waitAction(Duration.ofMillis(duration))
+//	                    .moveTo(startX, endY)
+//	                    .release()
+//	                    .perform();
+//	            break;
+//
+//
+//	        case DOWN:
+//	            startY = (int) (size.height * 0.70);
+//	            endY = (int) (size.height * 0.30);
+//	            startX = (size.width / 2);
+//	            new TouchAction(driver)
+//	                    .press(startX, startY)
+//	                    .waitAction(Duration.ofMillis(duration))
+//	                    .moveTo(startX, endY)
+//	                    .release()
+//	                    .perform();
+//
+//	            break;
+//
+//	    }
+//	}
 }
--- src/test/java/ABPNews_live/LiveTv.java
+++ src/test/java/ABPNews_live/LiveTv.java
@@ -23,7 +23,10 @@ public class LiveTv extends BaseClass{
 			WebDriverUtility wdu = new WebDriverUtility();
 			driver.manage().timeouts().implicitlyWait(30, TimeUnit.SECONDS);
 			driver.findElement(By.xpath("//android.widget.TextView[@text='(English)']")).click();
-			driver.findElement(By.id("com.android.packageinstaller:id/permission_allow_button")).click();
+			driver.findElement(By.id("com.android.permissioncontroller:id/permission_allow_foreground_only_button")).click();
+			driver.findElement(By.id("android:id/button2")).click();
+			
+//			driver.findElement(By.id("com.android.packageinstaller:id/permission_allow_button")).click();
 			WebElement ele = driver.findElement(By.xpath("//androidx.appcompat.app.ActionBar.Tab[@content-desc=\"For You\"]/android.widget.LinearLayout/android.widget.TextView"));
 			if (ele.isSelected()) {
 				WebElement home = driver.findElement(By.xpath("//androidx.appcompat.app.ActionBar.Tab[@content-desc=\"Home\"]/android.widget.LinearLayout/android.widget.TextView"));
@@ -32,13 +35,22 @@ public class LiveTv extends BaseClass{
 			Thread.sleep(5000);
 			driver.swipe(513, 585, 532, 1258, 500);
 			//driver.wait(25000);
-			Thread.sleep(25000);
-			driver.findElement(By.id("com.winit.starnews.hin:id/exo_subtitles")).click();
+			Thread.sleep(6000);
+			
+			driver.findElement(By.xpath("//android.widget.Button[@text='Skip Ad']")).click();
+//			driver.findElement(By.id("com.winit.starnews.hin:id/exo_subtitles")).click();
 			//wdu.TapOnElement(ele1);
 			
 			driver.pressKeyCode(AndroidKeyCode.KEYCODE_MEDIA_PAUSE);
+			Thread.sleep(5000);
 			driver.pressKeyCode(AndroidKeyCode.KEYCODE_MEDIA_PLAY);
+			driver.findElement(By.id("com.winit.starnews.hin:id/mute_btn")).click();
+			driver.findElement(By.id("com.winit.starnews.hin:id/mute_btn")).click();
 			
+			
+//			Thread.sleep(5000);
+			
+//			driver.findElement(By.id("com.winit.starnews.hin:id/exo_subtitles")).click();
 			LiveTV lt = new LiveTV(driver);
 //			driver.findElement(By.id("com.winit.starnews.hin:id/stream_quality_icon")).click();
 //			driver.findElement(By.xpath("//android.widget.TextView[@text='180p']")).click();
@@ -54,10 +66,19 @@ public class LiveTv extends BaseClass{
 			
 			lt.qualityic();
 			lt.lowres();
+			
+			Thread.sleep(5000);
+			driver.findElement(By.id("com.winit.starnews.hin:id/exo_subtitles")).click();
 			lt.qualityic();
 			lt.mediumres();
+			
+			Thread.sleep(5000);
+			driver.findElement(By.id("com.winit.starnews.hin:id/exo_subtitles")).click();
 			lt.qualityic();
 			lt.highres();
+			
+			Thread.sleep(5000);
+			driver.findElement(By.id("com.winit.starnews.hin:id/exo_subtitles")).click();
 			lt.qualityic();
 			lt.autores();
 			
@@ -69,3 +90,13 @@ public class LiveTv extends BaseClass{
 		
 		}
 }
+
+
+
+
+
+
+
+
+
+
